9b732e999beb63397ad0874e6b9d3e617b5d4fcd
jefGit <jefgit@gmail.com>
2019-07-28 23:00:16 +0430

    add irazasyed/telegram-bot-sdk

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * this apps  for me :)
+         */
+
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +231,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Telegram'  => Telegram\Bot\Laravel\Facades\Telegram::class
+
 
     ],
 
