3b88ac680650d2ca9174248798057a4871f5edd4
JefTab <jefTab@gmail.com>
2019-07-29 16:35:27 +0430

    fix image url salam command

--- app/EqtedarCommands/SalamCommand.php
+++ app/EqtedarCommands/SalamCommand.php
@@ -36,6 +36,6 @@ class SalamCommand extends Command
         // This will update the chat status to typing...
         $this->replyWithChatAction(['action' => Actions::TYPING]);
 
-        $this->replyWithMessage(['text' => $this->arguments]);
+        $this->replyWithMessage(['text' => $arguments]);
     }
 }
