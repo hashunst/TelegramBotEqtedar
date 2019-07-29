2977440907fad856cac8b1752246960cb749f905
JefTab <jefTab@gmail.com>
2019-07-29 16:59:14 +0430

    fix image url salam command

--- app/EqtedarCommands/SalamCommand.php
+++ app/EqtedarCommands/SalamCommand.php
@@ -53,7 +53,7 @@ class SalamCommand extends Command
         $response = Telegram::sendMessage([
             'chat_id' => $this->update->getMessage()->getChat()->getId(),
             'text' => 'Hello World',
-//            'reply_markup' => $reply_markup
+            'reply_markup' => $reply_markup
         ]);
 
         $messageId = $response->getMessageId();
