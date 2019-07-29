ad11bf0f59cd393915f8c820748d7dc3a0abe41d
JefTab <jefTab@gmail.com>
2019-07-29 15:28:13 +0430

    commands handlers

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,18 @@ Route::any('/test', function (){
 //    Telegram::sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
 //    file_put_contents(public_path('has'), 'has');
         $telegram = new Api();// webhook.php
-        $update = $telegram->commandsHandler(true);
-//        $telegram->sendMessage([
-//            'text' => $update->toJson(),
-//            'chat_id' => '146541735'
-//        ]);
+        $text = $telegram->commandsHandler(true)->getMessage()->getText();
+
+        if(!empty($text)){
+            $telegram->sendMessage([
+                'text' => $text,
+                'chat_id' => '146541735'
+            ]);
+        }
 //        $telegram
 ////            ->setAsyncRequest(true)
 //            ->sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
 //        return $telegram->getMe();
-        return $update->toArray();
+        return $text;
         return 'this is a test';
 });
