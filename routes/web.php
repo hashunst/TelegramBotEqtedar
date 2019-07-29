cbeb779fd35e51969f6a5963697785d9b592d46f
JefTab <jefTab@gmail.com>
2019-07-29 13:05:35 +0430

    test send photo

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', function () {
 Route::get('/test', function (){
 
     $telegram = new Api();
+    $telegram
+        ->setAsyncRequest(true)
+        ->sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
     return $telegram->getMe();
     return 'this is a test';
 });
