d21c12ebc9c82c4096301ab2cb291a9f15ff5567
JefTab <jefTab@gmail.com>
2019-07-29 14:22:42 +0430

    start command test 3

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ Route::get('/', function () {
 });
 
 Route::any('/test', function (){
-
-        $telegram = new Api();
-        $telegram
-//            ->setAsyncRequest(true)
-            ->sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
-        return $telegram->getMe();
+    Telegram::sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
+//        $telegram = new Api();
+//        $telegram
+////            ->setAsyncRequest(true)
+//            ->sendPhoto(['chat_id' => '146541735', 'photo' => resource_path('download.png')]);
+//        return $telegram->getMe();
         return 'this is a test';
 });
